52e812780a8c6b57d89fa0c4a2be24d146931d75
JonatanUmul <jumuln@miumg.edu.gt>
2024-04-12 00:16:05 -0600

    otra vez

--- frontend/src/components/dashbords/Dashboard.jsx
+++ frontend/src/components/dashbords/Dashboard.jsx
@@ -4,21 +4,21 @@ import './estilos.css'
 
 const Dashboard = () => {
   return (
-    <div>
+   
       
-    <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  
     <iframe
       title="reporteProduccion"
-      width="800"
+      width="100%"
       height="480"
       src="https://app.powerbi.com/view?r=eyJrIjoiMjIwYzFjZjEtYzcwMi00MzczLTk4ODMtOTFkYjYxOTMzNGViIiwidCI6ImNmMjViNGI0LTQzOWYtNGEwYy1iOTg0LWI1NGU0OGMzMmNlNCJ9"
       frameBorder="0"
       allowFullScreen="true"
       style={{ border: '1px solid #ccc', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}
     ></iframe>
-  </div>
+
      
-    </div>
+ 
   );
 };
 
