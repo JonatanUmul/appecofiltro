65a0ba1da848165e47db1c0d79d14aa1feec5333
JonatanUmul <jumuln@miumg.edu.gt>
2024-04-12 00:22:11 -0600

    asa
    as

--- frontend/src/components/dashbords/Dashboard.jsx
+++ frontend/src/components/dashbords/Dashboard.jsx
@@ -10,13 +10,12 @@ const Dashboard = () => {
     <iframe
       title="reporteProduccion"
       width="100%"
-      height="480"
+      height="1060"
       src="https://app.powerbi.com/view?r=eyJrIjoiMjIwYzFjZjEtYzcwMi00MzczLTk4ODMtOTFkYjYxOTMzNGViIiwidCI6ImNmMjViNGI0LTQzOWYtNGEwYy1iOTg0LWI1NGU0OGMzMmNlNCJ9"
       frameBorder="0"
       allowFullScreen="true"
       style={{ border: '1px solid #ccc', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}
     ></iframe>
-
      
  
   );
