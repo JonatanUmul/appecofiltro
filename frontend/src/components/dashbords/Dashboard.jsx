c5fc399af1b62da3057ed91a5ad5762b4204dc4b
JonatanUmul <jumuln@miumg.edu.gt>
2024-04-12 00:02:50 -0600

    actualizaxion

--- frontend/src/components/dashbords/Dashboard.jsx
+++ frontend/src/components/dashbords/Dashboard.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import DasAserrin from './DasAserrin';
-import PropsHornos from './PropsHornos';
+
 import './estilos.css'
-import Clima from './Clima';
+
 const Dashboard = () => {
   return (
     <div>
       
-        <PropsHornos />
-   
+    <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <iframe
+      title="reporteProduccion"
+      width="800"
+      height="480"
+      src="https://app.powerbi.com/view?r=eyJrIjoiMjIwYzFjZjEtYzcwMi00MzczLTk4ODMtOTFkYjYxOTMzNGViIiwidCI6ImNmMjViNGI0LTQzOWYtNGEwYy1iOTg0LWI1NGU0OGMzMmNlNCJ9"
+      frameBorder="0"
+      allowFullScreen="true"
+      style={{ border: '1px solid #ccc', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}
+    ></iframe>
+  </div>
      
     </div>
   );
