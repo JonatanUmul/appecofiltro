9e395e8fd98dd0583899f62812ca133f0bee8d09
JonatanUmul <jumuln@miumg.edu.gt>
2024-06-08 23:14:48 -0600

    actualizacioneas varias
    actualizacioneas varias

--- frontend/src/components/dashbords/Dashboard.jsx
+++ frontend/src/components/dashbords/Dashboard.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-
-import './estilos.css'
+import Hornos from './Hornos';
+import './estilos.css'; // Asegúrate de que este archivo CSS existe
 
 const Dashboard = () => {
   return (
-   
-      
-  
-    <iframe
-      title="reporteProduccion"
-      width="100%"
-      height="1060"
-      src="https://app.powerbi.com/view?r=eyJrIjoiMjIwYzFjZjEtYzcwMi00MzczLTk4ODMtOTFkYjYxOTMzNGViIiwidCI6ImNmMjViNGI0LTQzOWYtNGEwYy1iOTg0LWI1NGU0OGMzMmNlNCJ9"
-      frameBorder="0"
-      allowFullScreen="true"
-      style={{ border: '1px solid #ccc', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}
-    ></iframe>
+    <div>
      
- 
+      <Hornos />
+    </div>
   );
 };
 
